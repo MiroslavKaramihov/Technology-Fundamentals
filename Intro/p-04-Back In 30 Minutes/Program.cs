95f7983a3092aa27da7efb7f9235cbdf29c23b55
Miroslav Kostov Karamihov <45258645+MiroslavKaramihov@users.noreply.github.com>
2018-12-19 10:27:50 +0200

    FirstSteps

---
+++ Intro/p-04-Back In 30 Minutes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace p_04_Back_In_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minets = int.Parse(Console.ReadLine())+30;
+
+            if (minets>59)
+            {
+                minets -= 60;
+                hours++;
+            }
+            if (hours>23)
+            {
+                hours = 0;
+            }
+            Console.WriteLine($"{hours}:{minets:d2}");
+        }
+    }
+}
